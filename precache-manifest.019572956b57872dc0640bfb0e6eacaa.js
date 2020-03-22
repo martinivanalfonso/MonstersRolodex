10dfb6628c8f6bd95c7e9c61fc6cd5d2f9664c63
martinivanalfonso <martinivanalfonso@gmail.com>
2020-03-22 18:20:51 -0300

    Updates

---
+++ precache-manifest.019572956b57872dc0640bfb0e6eacaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc9c8b2fee0f571ae4f2d3db35419aaa",
+    "url": "/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "dc2a3fbc31be4c1ede79",
+    "url": "/MonstersRolodex/static/css/main.bfa4542b.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/MonstersRolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonstersRolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc2a3fbc31be4c1ede79",
+    "url": "/MonstersRolodex/static/js/main.aa731020.chunk.js"
+  },
+  {
+    "revision": "0356c89559ba280291dc",
+    "url": "/MonstersRolodex/static/js/runtime-main.ee30fdb1.js"
+  }
+]);
